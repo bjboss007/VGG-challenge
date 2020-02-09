52bd6322e1f04c96ca99bb233b06466ee17bb2f1
Habib <habib.muhammad@longbridgetech.com>
2020-02-09 22:28:16 +0100

    House Cleaning

--- run.py
+++ run.py
@@ -1,9 +1,14 @@
 from challenge import create_app
-
-
+from challenge.models import Project
+from challenge import db
 
 
 app = create_app()
-
+app.app_context().push()
 if __name__ == "__main__":
+    
+    try:
+        counter = len(Project.query.all()) 
+    except Exception:
+        db.create_app()
     app.run(port = 8083)
