3accada05d049e309dede2f33655577822c8ad1a
Muhammad Habib <mobolajihabib@gmail.com>
2020-02-10 12:08:54 +0100

    Update config.py
    Updated the configuration file

--- challenge/config.py
+++ challenge/config.py
@@ -2,15 +2,15 @@ import os
 
 
 class Config():
-    SECRETKEY = 'eab3cbb80cc6459864c930ebd9d29dea'
+    SECRETKEY = os.environ.get('SECRETKEY')
     SQLALCHEMY_DATABASE_URI = "sqlite:///database.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = True
-    JWT_SECRET_KEY = '7d9b829544b4c258cb91ddfd4b5a9801'
+    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
 
 
 class Production(Config):
-    SECRETKEY = 'eab3cbb80cc6459864c930ebd9d29dea'
-    SQLALCHEMY_DATABASE_URI = "postgres://svklzrvsnbcqqt:eb5fde5a027be5ea0059bb9d5bb328950e6d228010e9076fba3000d6631452dd@ec2-18-213-176-229.compute-1.amazonaws.com:5432/db66hh7reqtt87"
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     DEBUG = False
     
+   
