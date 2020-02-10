2ba1e20bff0f95f294ce646b77319be20486d6c4
Muhammad Habib <mobolajihabib@gmail.com>
2020-02-10 21:12:29 +0100

    Update config.py

--- challenge/config.py
+++ challenge/config.py
@@ -3,7 +3,7 @@ import os
 
 class Config():
     SECRETKEY = os.environ.get('SECRETKEY')
-    SQLALCHEMY_DATABASE_URI = "sqlite:///database.db"
+#     SQLALCHEMY_DATABASE_URI = "sqlite:///database.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = True
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
